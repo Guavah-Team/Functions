a54477eb7fa9667ce4ed50f1ac4682ffd7365682
HovagApelian <70242013+HovagApelian@users.noreply.github.com>
2022-03-22 11:09:22 -0700

    Create add-users-to-db.py

---
+++ add-users-to-db.py
@@ -0,0 +1,39 @@
+import json
+import boto3
+from time import gmtime, strftime
+
+#Write user data to the GuavahUsers table in DynamoDB
+def put_user(user_ID, name, email, dt):
+    dynamodb = boto3.resource('dynamodb')
+
+    table = dynamodb.Table('GuavahUsers')
+    response = table.put_item(
+       Item={
+            'UserID': user_ID,
+            'Name': name,
+            'Email': email,
+            'CreatedAt': dt,
+            'Reviews': []
+        }
+    )
+    return response
+    
+    
+
+def lambda_handler(event, context):
+    
+    #Get user info from Cognito event
+    user_ID = event['userName']
+    name = event['request']['userAttributes']['name']
+    email = event['request']['userAttributes']['email']
+    
+    #Current date
+    dt = strftime("%Y-%m-%d %H:%M:%S", gmtime())
+    dt = str(dt)
+    
+    #Writes to DynamoDB
+    #NOTICE: A new user account will be successffully created despite this function failing.
+    #If this function fails, user data will NOT be available in DynamoDB.
+    put_user(user_ID, name, email, dt)
+   
+    return event
