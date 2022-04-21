bf1a1374fa55b348ff84e7447a2e4f987597f762
HovagApelian <70242013+HovagApelian@users.noreply.github.com>
2022-04-21 16:21:20 -0700

    Update guavahGetTableSizes.py

--- guavahGetTableSizes.py
+++ guavahGetTableSizes.py
@@ -1,14 +1,13 @@
 import json
 import boto3
 
-
 def lambda_handler(event, context):
-
+    
     dynamodb = boto3.resource('dynamodb')
     table = dynamodb.Table('guavahTableCounter')
-
+    
     response = table.scan()
-
+    
     return {
         'statusCode': 200,
         'body': response
